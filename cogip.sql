751d7aadb0d7cffc0d8607e26719a96be8d44384
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-02-01 15:26:22 +0100

    Add database values

--- cogip.sql
+++ cogip.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS cogip;
 
 USE cogip;
 
-CREATE TABLE types (
+CREATE TABLE IF NOT EXISTS types (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -10,7 +10,7 @@ CREATE TABLE types (
 );
 
 
-CREATE TABLE companies(
+CREATE TABLE IF NOT EXISTS companies(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
   type_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE companies(
   updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE invoices(
+CREATE TABLE IF NOT EXISTS invoices(
   id INT AUTO_INCREMENT PRIMARY KEY,
   ref VARCHAR(50),
   id_company INT,
@@ -30,7 +30,7 @@ CREATE TABLE invoices(
   updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE contacts(
+CREATE TABLE IF NOT EXISTS contacts(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
   company_id INT,
@@ -41,21 +41,21 @@ CREATE TABLE contacts(
   updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE roles(
+CREATE TABLE IF NOT EXISTS roles(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE permissions(
+CREATE TABLE IF NOT EXISTS permissions(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE roles_permission(
+CREATE TABLE IF NOT EXISTS roles_permission(
   id INT AUTO_INCREMENT PRIMARY KEY,
   permission_id INT,
   FOREIGN KEY(permission_id) REFERENCES permissions(id),
@@ -63,7 +63,7 @@ CREATE TABLE roles_permission(
   FOREIGN KEY(role_id) REFERENCES roles(id)
 );
 
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(50),
   role_id INT,
@@ -74,3 +74,44 @@ CREATE TABLE users(
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
+
+INSERT INTO types (name)
+VALUES ("supplier"), ("client");
+
+INSERT INTO companies (name, type_id, country, tva) 
+VALUES 
+      ("Raviga", 1, "United States", "US456654321"),
+      ("Dunder Miffilin", 2, "United States", "US676787767"),
+      ("Pierre Cailloux", 1, "France", "FR 676 676 676"),
+      ("Belgalol", 1, "Belgium", "BE0987 876 787"),
+      ("Jouet Jean-Michel", 2, "France", "FR787 776 999");
+
+INSERT INTO invoices (ref, id_company)
+VALUES 
+      ("F20220915-001", 5),
+      ("F20220915-002", 2),
+      ("F20220915-003", 3),
+      ("F20220915-004", 1),
+      ("F20220915-005", 4);
+
+INSERT INTO contacts (name, company_id, email, phone)
+VALUES
+      ("Peter Gregory", 1, "peter.gregory@raviga.com","555-4567"),
+      ("Cameron How", 2, "cam.how@dundermiffilin.com","555-8765"),
+      ("Gavin Belson", 3, "gavin@pcailloux.com","555-6354"),
+      ("Jian Yang", 4, "jian.yan@belgalol.com","555-4567"),
+      ("Bertram Gilfoy", 5, "bertram.gilfoyle@jjeanmichel.com","555-5434");
+
+INSERT INTO roles (name)
+VALUES ("Admin"), ("moderator");
+
+INSERT INTO permissions (name)
+VALUES ("create"), ("read"), ("update"), ("delete");
+
+INSERT INTO roles_permission (permission_id, role_id)
+VALUES (1, 1), (2, 1), (3, 1), (4, 1), (2,2), (1,2);
+
+INSERT INTO users (first_name, last_name, email, password, role_id)
+VALUES 
+      ("Jean-Christian", "Ranu", "jc.ranu@cogip.com", "ranu", 1),
+      ("Murriel", "Perrache", "murriel-perrache@cogip.com", "perrache", 2);
