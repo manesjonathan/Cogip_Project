fb6e0ebc9ca97d217d751d23956b8536d5aa717a
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-02-02 13:42:00 +0100

    Create functions

--- Services/CompanyService.php
+++ Services/CompanyService.php
@@ -14,22 +14,67 @@ class CompanyService
     }
 
 
-//function createCompany($array)
-//function getLastFiveCompanies() return array
-//function getCompanyById($id) return Company
+    public function createCompany($array)
+    {
 
-//function createContact($array)
-//function getAllContacts() return array of Company
-//function getContactById($id) return Contact
-//function getAllContactsByCompany($company) return array of Contact
-//function getLastFiveContactsByCompany($company) return array of contact
+        return; // Todo: return boolean true on succes, false on failure
+    }
+    
+    public function getLastFiveCompanies()
+    {
+        return; // Todo: return array
+    } 
+    
+    public function getCompanyById($id){
+        return; // Todo: return company array
+    }
 
-//function createInvoice($array)
-//function getInvoiceById($id) return Invoice
-//function getLastFiveInvoicesByCompany($company) return array of invoices
-//function getInvoicesByCompany($company) return array of invoices
+    public function createContact($array)
+    {
+        return; // Todo: return boolean true on succes, false on failure
+    }
 
-    //function getData() -> return to frontend if user is valid
+    public function getAllContacts()
+    {
+        return; // Todo: return Array of company
+    } 
+
+    public function getContactById($id)
+    {
+        return; // Todo: return array contact
+    }
+
+    public function getAllContactsByCompany($company)
+    {
+        return; // Todo: return array of all contacts
+    }
+
+    public function getLastFiveContactsByCompany($company)
+    {
+        return; // Todo: return array of contact
+    }
+        
+    public function createInvoice($array)
+    {
+        return; // Todo: return boolean true on succes, false on failure
+    }
+
+    public function getInvoiceById($id) 
+    {
+        return; // Todo: retrun Invoice array
+    }
+
+    public function getLastFiveInvoicesByCompany($company)
+    {
+        return; // Todo: return array of invoices
+    } 
+
+    public function getInvoicesByCompany($company)
+    {
+        return; // Todo: return array of invoices
+    }
+
+    // function getData() -> return to frontend if user is valid
     //return json object containing 3 arrays (last 5 invoices, last 5 contact, last 5 companies)
     public function getData()
     {
