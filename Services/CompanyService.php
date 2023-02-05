b3f5af7cd887e4458f0fd1ede0ca8bdb93e3fb76
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-02-05 17:46:55 +0100

    Add id Verification logic

--- Services/CompanyService.php
+++ Services/CompanyService.php
@@ -17,7 +17,7 @@ class CompanyService
     public function createCompany($array)
     {
 
-        return; // Todo: return boolean true on succes, false on failure
+        return;
     }
     
     public function getLastFiveCompanies()
@@ -30,6 +30,16 @@ class CompanyService
     } 
     
     public function getCompanyById($id){
+        if (!is_numeric($id)) {
+            header('Content-type: application/json');
+            http_response_code(400);
+
+            echo json_encode(["status" => "Bad Request"]);
+            exit();
+        }
+        $id = intval($id);
+        
+        $data["status"] = "ok";
         $data["company"] = $this->company_repository->getCompanyById($id);
         $json_encode = json_encode($data, true);
 
@@ -53,6 +63,15 @@ class CompanyService
 
     public function getContactById($id)
     {
+        if (!is_numeric($id)) {
+            header('Content-type: application/json');
+            http_response_code(400);
+
+            echo json_encode(["status" => "Bad Request"]);
+            exit();
+        }
+        $id = intval($id);
+
         $data["contact"] = $this->company_repository->getCompanyById($id);
         $json_encode = json_encode($data, true);
         
@@ -62,6 +81,15 @@ class CompanyService
 
     public function getAllContactsByCompany($company)
     {
+        if (!is_numeric($company)) {
+            header('Content-type: application/json');
+            http_response_code(400);
+
+            echo json_encode(["status" => "Bad Request"]);
+            exit();
+        }
+        $company = intval($company);
+
         $data["contacts"] = $this->company_repository->getAllContactsByCompany($company);
         $json_encode = json_encode($data, true);
         
@@ -71,6 +99,15 @@ class CompanyService
 
     public function getLastFiveContactsByCompany($company)
     {
+        if (!is_numeric($company)) {
+            header('Content-type: application/json');
+            http_response_code(400);
+
+            echo json_encode(["status" => "Bad Request"]);
+            exit();
+        }
+        $company = intval($company);
+
         $data["contacts"] = $this->company_repository->getLastFiveContactsByCompany($company);
         $json_encode = json_encode($data, true);
         
@@ -85,6 +122,14 @@ class CompanyService
 
     public function getInvoiceById($id) 
     {
+        if (!is_numeric($id)) {
+            header('Content-type: application/json');
+            http_response_code(400);
+
+            echo json_encode(["status" => "Bad Request"]);
+            exit();
+        }
+        $id = intval($id);
         $data["invoice"] = $this->company_repository->getInvoiceById($id);
         $json_encode = json_encode($data, true);
         
@@ -94,6 +139,15 @@ class CompanyService
 
     public function getLastFiveInvoicesByCompany($company)
     {
+        if (!is_numeric($company)) {
+            header('Content-type: application/json');
+            http_response_code(400);
+
+            echo json_encode(["status" => "Bad Request"]);
+            exit();
+        }
+        $company = intval($company);
+
         $data["invoices"] = $this->company_repository->getLastFiveInvoicesByCompany($company);
         $json_encode = json_encode($data, true);
         
@@ -103,6 +157,15 @@ class CompanyService
 
     public function getInvoicesByCompany($company)
     {
+        if (!is_numeric($company)) {
+            header('Content-type: application/json');
+            http_response_code(400);
+
+            echo json_encode(["status" => "Bad Request"]);
+            exit();
+        }
+        $company = intval($company);
+        
         $data["invoices"] = $this->company_repository->getInvoicesByCompany($company);
         $json_encode = json_encode($data, true);
         
