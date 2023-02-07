fe46d5079db7b69087eb11e5e7d932376f1be779
Marnie Benalia <marnie.benalia@gmail.com>
2023-02-07 12:10:36 +0100

    début de projet

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,8 +1,23 @@
 function Burger() {
 
-      return (
-        <h1>Bonjour</h1>)
-
-    }
+return (
+    <nav className="ham-menu">
+    <button className="hm-button" aria-controls="primary-navigation" aria-expanded="false">
+        <svg class="ham" viewbox=" 0 0 100 100" width="40">
+            <rect className="line top" 
+                  width="30" height="5"
+                   x="10" y="20" rx="5"> 
+            </rect>
+            <rect className="line middle" 
+                  width="30" height="5"
+                  x="10" y="30" rx="5">
+            </rect>
+            <rect className="line bottom" 
+                  width="30" height="5"
+                  x="10" y="40" rx="5">
+            </rect>
+        </svg> 
+        </button>
+        </nav>)};
 
 export default Burger;
