5a0b1e02ba32cec579cdf1be6fc89bca7039fe35
aurorelem <aurorelemaire.pro@gmail.com>
2023-02-08 15:54:42 +0100

    Footer.js fin de journée

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import '../styles/styles.css';
 import Header from './Header';
 import DisplayArray from './DisplayArray';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <DisplayArray url={"https://catfact.ninja/facts"}/>
+      <Footer />
     </div>
   );
 }
