508cfb647809b42e49ca5624800ac62a2ae252c0
MarnieBenalia <marnie.benalia@gmail.com>
2023-02-08 15:50:35 +0100

    workday

--- src/components/PanneauHeader.js
+++ src/components/PanneauHeader.js
@@ -5,7 +5,7 @@ const PanneauHeader = () => {
 
 return (
 
-  <nav class="header-container">
+  <nav class="panneau-container">
       <h2 className="title-panneau">Manage your customers and invoices easly</h2>
       <img className ="img-panneau" src={panneau} alt="img-panneau" ></img>
   </nav>
