8900e7af4152d535429790380b78232726c2ff95
MarnieBenalia <marnie.benalia@gmail.com>
2023-02-08 10:16:23 +0100

    workday

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import Burger from './Burger';
 
 const Header = () => {
   return (
     <header className='header'>
 
     <nav className="menu-container">
-        <nav className="test">
+
           <h1 className="menu-title">COGIP</h1>
-          <Burger />
-        </nav>
+
+          <nav className="ham-menu">
+                  <button className="hm-button" aria-controls="primary-navigation" aria-expanded="false">
+                        <svg className="ham" viewBox="0 0 100 100" width="50">
+                        <rect className="line top" width="80" height="10" x="10" y="25" rx="5" />
+                        <rect className="line middle" width="80" height="10" x="10" y="45" rx="5" />
+                        <rect className="line bottom" width="80" height="10" x="10" y="65" rx="5" />
+                        </svg>
+                  </button>
+            </nav>
+
         <ul className="menu-list">
             <li>Home</li>
             <li>Invoices</li>
@@ -19,8 +27,10 @@ const Header = () => {
             <li>Contacts</li>
         </ul>
 
-            <button className="button-sign-up">Login</button>
-            <button className="button-login">Sign up</button>
+        <ul className="log">          
+          <li>login</li>
+          <li>signup</li>
+        </ul>
     </nav>
     </header>
   );
