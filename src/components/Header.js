e29e65578f3c45eb2559c3f2fa55e956e2ec7cd8
Marnie Benalia <marnie.benalia@gmail.com>
2023-02-09 16:21:13 +0100

    début de projet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,49 @@
 import React, { useState } from 'react';
 
 const Header = () => {
- const [isOpened, setIsOpened] = useState(false);
 
-  return (
+const [isOpened, setIsOpened] = useState(false);
+return (
 
-  <header className='header'>
+<header className='header'>
+<nav className="menu-container">
 
-  <nav className="menu-container">
+<h1 className="menu-title">COGIP</h1>
 
-          <h1 className="menu-title">COGIP</h1>
-          <nav className="ham-menu">
-                  <button className="hm-button" aria-controls="primary-navigation" aria-expanded="false" aria-expended={isOpened} onClick={() => setIsOpened(!isOpened)} >
-                        <svg className="ham" viewBox="0 0 100 100" width="50">
-                        <rect className="line top" width="80" height="10" x="10" y="25" rx="5" />
-                        <rect className="line middle" width="80" height="10" x="10" y="45" rx="5" />
-                        <rect className="line bottom" width="80" height="10" x="10" y="65" rx="5" />
-                        </svg>
-                  </button>
-            </nav>
 
-<nav className='list' style={{ marginLeft: isOpened ? '0%' : '-200%' }}>
-        <ul className="menu-list">
+        <ul className="menu menu-list">
             <li>Home</li>
             <li>Invoices</li>
             <li>Compagnies</li>
             <li>Contacts</li>
         </ul>
-      <ul className="menu-log">
+      <ul className="menu menu-log">
         <li className="menu-signup">Sign up</li>
         <li>login</li>
         </ul>
-        </nav>
-  </nav>
+
+
+    <nav className="ham-menu">
+        <button className="hm-button" aria-controls="primary-navigation" aria-expanded="false" aria-expended={isOpened} onClick={() => setIsOpened(!isOpened)}>
+            <svg className="ham" viewBox="0 0 100 100" width="50">
+              <rect className="line top" width="80" height="10" x="10" y="25" rx="5" />
+              <rect className="line middle" width="80" height="10" x="10" y="45" rx="5" />
+              <rect className="line bottom" width="80" height="10" x="10" y="65" rx="5" />
+            </svg>
+          </button>
+    </nav>
+
+    <ul className="menu-list-mobile" style={{ marginLeft: isOpened ? '0%' : '-200%' }}>
+        <li>Home</li>
+        <li>Invoices</li>
+        <li>Compagnies</li>
+        <li>Contacts</li>
+        <li>Sign up</li>
+        <li>login</li>
+    </ul>
+</nav>
 </header>
-  );
-};
+
+)}
 
 export default Header;
