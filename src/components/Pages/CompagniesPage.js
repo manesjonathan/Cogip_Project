05b2a30e835b4f3fd00af3450912546c598c7b45
MarnieBenalia <marnie.benalia@gmail.com>
2023-02-10 14:34:56 +0100

    rajout de react router

--- src/components/Pages/CompagniesPage.js
+++ src/components/Pages/CompagniesPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from '../Elements/Footer.js';
 import Header from '../Elements/Header.js';
 
 const CompagniesPage = () => {
 
 return (
-    <Header />
+    <div>
+        <Header />
+        <Footer/>
+    </div>
 )}
 
 export default CompagniesPage;
--- src/components/Pages/InvoicesPage.js
+++ src/components/Pages/InvoicesPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from '../Elements/Footer.js';
 import Header from '../Elements/Header.js';
 
 const InvoicesPage = () => {
 
 return (
-    <Header />
+    <div>
+        <Header />
+        <Footer/>
+    </div>
 )}
 
 export default InvoicesPage;
