50760e33e1574335f5981812527452a14435c34c
aurorelem <aurorelemaire.pro@gmail.com>
2023-02-14 16:40:04 +0100

    modifs footer

--- src/components/Elements/Footer.js
+++ src/components/Elements/Footer.js
@@ -11,79 +11,74 @@ import Googleplus from "../../Images/googleplus.avif";
 import Pinterest from "../../Images/pinterest.avif";
 import RSS from "../../Images/rss.avif";
 
-
 const Footer = () => {
   return (
     <footer classeName="footer">
       <nav className="social-container">
-        <h1 className="footer-title">COGIP</h1>
-        <ul className="social-contact">
-          <li>
-            <img className="logo-location" 
-            src={Location} 
-            alt="logo location" />
-            Square des Martyrs, 6000 Charleroi
-          </li><br />
-          <li>
-            <img className="logo-phone" 
-            src={Phone} 
-            alt="logo phone" />
-            (123)456-7890<br />
-          <li>
-          </li>
-            <img className="logo-printer" 
-            src={Printer} 
-            alt="logo printer " />
-            (123)456-7890
-          </li>
-        </ul>
-        <ul className="social-media">
-          <h4>Social Media</h4>
-          <li>
-            <img className="logo-facebook" 
-            src={Facebook} 
-            alt="logo facebook" />
-          </li>
-          <li>
-            <img className="logo-twitter" 
-            src={Twitter} 
-            alt="logo twitter" />
-          </li>
-          <li>
-            <img className="logo-linkedin" 
-            src={Linkedin} 
-            alt="logo linkedin" />
-          </li>
-          <li>
-            <img className="logo-youtube" 
-            src={Youtube} 
-            alt="logo youtube" />
-          </li>
-          <li>
-            <img className="logo-instagram"
-            src={Instagram} 
-            alt="logo instagram" />
-          </li>
-          <li>
-            <img
-              className="logo-googleplus"
-              src={Googleplus}
-              alt="logo googleplus"
-            />
-          </li>
-          <li>
-            <img
-              className="logo-pinterest"
-              src={Pinterest}
-              alt="logo pinterest"
-            />
-          </li>
-          <li>
-            <img className="logo-rss" 
-            src={RSS} 
-            alt="logo rss" />
-          </li>
-        </ul>
+        <div className="wrapper-title">
+          <h1 className="footer-title">COGIP</h1>
+        </div>
+        <div classeName="social-div">
+          <img className="logo-location" src={Location} alt="logo location" />
+          Square des Martyrs, 6000 Charleroi
+          <ul className="social-contact">
+            <li>
+              <img className="logo-phone" src={Phone} alt="logo phone" />
+              (123)456-7890
+            </li>
+            <li>
+              <img className="logo-printer" src={Printer} alt="logo printer " />
+              (123)456-7890
+            </li>
+          </ul>
+          <ul className="social-media">
+            <h4>Social Media</h4>
+            <li>
+              <img
+                className="logo-facebook"
+                src={Facebook}
+                alt="logo facebook"
+              />
+            </li>
+            <li>
+              <img className="logo-twitter" src={Twitter} alt="logo twitter" />
+            </li>
+            <li>
+              <img
+                className="logo-linkedin"
+                src={Linkedin}
+                alt="logo linkedin"
+              />
+            </li>
+            <li>
+              <img className="logo-youtube" src={Youtube} alt="logo youtube" />
+            </li>
+            <li>
+              <img
+                className="logo-instagram"
+                src={Instagram}
+                alt="logo instagram"
+              />
+            </li>
+            <li>
+              <img
+                className="logo-googleplus"
+                src={Googleplus}
+                alt="logo googleplus"
+              />
+            </li>
+            <li>
+              <img
+                className="logo-pinterest"
+                src={Pinterest}
+                alt="logo pinterest"
+              />
+            </li>
+            <li>
+              <img className="logo-rss" src={RSS} alt="logo rss" />
+            </li>
+          </ul>
+        </div>
       </nav>
       <nav className="menu-footer">
         <ul className="link-footer">
