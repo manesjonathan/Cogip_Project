f123afd6dcee0b98cceecd5816bf13647a43da58
Marnie Benalia <marnie.benalia@gmail.com>
2023-02-21 16:41:59 +0100

    workfinalday

--- src/components/Elements/HeaderHome.js
+++ src/components/Elements/HeaderHome.js
@@ -25,7 +25,7 @@ return (
 
 
     <nav className="ham-menu">
-        <button className="hm-button" aria-controls="primary-navigation" aria-expanded="false" aria-expended={isOpened} onClick={() => setIsOpened(!isOpened)}>
+        <button aria-label="Menu burger" className="hm-button" aria-controls="primary-navigation" aria-expanded="false" aria-expended={isOpened} onClick={() => setIsOpened(!isOpened)}>
             <svg className="ham" viewBox="0 0 100 100" width="50">
               <rect className="line top" width="80" height="10" x="10" y="25" rx="5" />
               <rect className="line middle" width="80" height="10" x="10" y="45" rx="5" />
